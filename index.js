5be5026e635278e68379762a26ae2690f313eec4
pekko1215 <s16t218@stu.kagawa-u.ac.jp>
2019-07-06 01:23:44 +0900

    TeamToBoard

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const Request = (()=>{
-    if(typeof window !== 'undefined'){
-        return (url)=>fetch(url).then(d=>d.json());
-    }else{
-        return (url)=>new Promise(r=>{
-            require('request')(url,(e,req,body)=>{
+const Request = (() => {
+    if (typeof window !== 'undefined') {
+        return (url) => fetch(url).then(d => d.json());
+    } else {
+        return (url) => new Promise(r => {
+            require('request')(url, (e, req, body) => {
                 r(JSON.parse(body));
             })
         })
     }
 })()
 const EndPoint = {
-    Base:'https://wb-api.glitch.me'
+    Base: 'https://wb-api.glitch.me'
 }
 
 EndPoint.Cups = EndPoint.Base + '/api/bi/';
 EndPoint.Board = EndPoint.Base + '/api/bd/';
 
 class Rank {
-    constructor(obj,team){
+    constructor(obj, team) {
         this.rankOrder = obj.rankOrder;
         this.userId = obj.userId;
         this.name = obj.name;
@@ -30,97 +30,98 @@ class Rank {
 }
 
 class Team {
-    constructor(obj){
+    constructor(obj, board) {
         this.id = obj.id;
         this.name = obj.name;
         this.is_all = obj.is_all;
+        this.board = board;
     }
 }
 
 class Board {
-    constructor(obj,cup){
+    constructor(obj, cup) {
         this.date = new Date(obj.dateFormatted);
         this.url = obj.url;
         this.cup = cup;
-        this.teams = obj.teams.map(obj=>new Team(obj));
-        this.ranks = obj.ranks.map((arr,i)=>{
-            return arr.map(d=>{
-                return new Rank(d,this.teams[i]);
+        this.teams = obj.teams.map(obj => new Team(obj, this));
+        this.ranks = obj.ranks.map((arr, i) => {
+            return arr.map(d => {
+                return new Rank(d, this.teams[i]);
             })
         });
-        this.teams.forEach((team,i)=>team.ranks = this.ranks[i]);
+        this.teams.forEach((team, i) => team.ranks = this.ranks[i]);
     }
 }
 
 class Cup {
-    constructor(obj){
+    constructor(obj) {
         this.title = obj.title;
         this.first = obj.first;
         this.last = obj.last;
         this.id = obj.id;
     }
-    async getBoardInfomation(query){
-        if(this.id == -1 || this.id === undefined) return null;
-        let url = EndPoint.Board + `${this.id}/`+query;
-        let data = await Request(url);
-        if(!data.result) return null;
-        data = data.data[0];
-        data.url = url;
-        data = new Board(data,this);
-        return data;
-    }
-    /**
-     *全取得。非常に重いので非推奨
-     */
-    async getBoardInfomationAll(){
-        return await this.getBoardInfomation('all');
-    }
-    /**
-     * XX:55のみ(ex: 17:55など)を取得。よほどのことがない限りこちらを推奨
-     */
-    async getBoardInfomationHours(date){
-        return await this.getBoardInfomation('hours');
-    }
-    /**
-     * 最終更新データを確認。
-     */
-    async getBoardInfomationLast(){
+    async getBoardInfomation(query) {
+            if (this.id == -1 || this.id === undefined) return null;
+            let url = EndPoint.Board + `${this.id}/` + query;
+            let data = await Request(url);
+            if (!data.result) return null;
+            data = data.data[0];
+            data.url = url;
+            data = new Board(data, this);
+            return data;
+        }
+        /**
+         *全取得。非常に重いので非推奨
+         */
+    async getBoardInfomationAll() {
+            return await this.getBoardInfomation('all');
+        }
+        /**
+         * XX:55のみ(ex: 17:55など)を取得。よほどのことがない限りこちらを推奨
+         */
+    async getBoardInfomationHours(date) {
+            return await this.getBoardInfomation('hours');
+        }
+        /**
+         * 最終更新データを確認。
+         */
+    async getBoardInfomationLast() {
         return await this.getBoardInfomation('last');
     }
 
     /**
      * (day+1)日目最終更新を取得。daylast/0なら1日目。
      */
-    async getBoardInfomationDaylast(day){
-        return await this.getBoardInfomation('daylast/'+day);
+    async getBoardInfomationDaylast(day) {
+        return await this.getBoardInfomation('daylast/' + day);
     }
 
     /**
      * ある時間におけるボードを取得。time/2019/02/28/15/50などと指定する。
      */
-    async getBoardInfomationTime(year = 0,month = 0,day = 0,hour = 0,min = 0){
+    async getBoardInfomationTime(year = 0, month = 0, day = 0, hour = 0, min = 0) {
         return await this.getBoardInfomation(`time/${year}/${month}/${day}/${hour}/${min}`)
     }
 
     /**
      * 特定ユーザーのデータのみを返します(全取得なので非推奨)
      */
-    async getBoardInfomationUser(userid){
-        return await this.getBoardInfomation('user/'+userid);
+    async getBoardInfomationUser(userid) {
+        return await this.getBoardInfomation('user/' + userid);
     }
 
     /**
      * 特定ユーザーのデータのみを、XX:55のものに絞って返します。
-     */    
-    async getBoardInfomationUserHours(userid){
+     */
+    async getBoardInfomationUserHours(userid) {
         return await this.getBoardInfomation(`user/${userid}/hours`);
     }
 }
 
-async function GetCups(){
+async function GetCups() {
     let data = await Request(EndPoint.Cups);
-    if(!data.result) return null;
-    data.cups = data.cups.filter(d=>d.result).map(d=>new Cup(d));
+    if (!data.result) return null;
+    data.cups = data.cups.filter(d => d.result).map(d => new Cup(d));
     data.now_cup = data.now_cup.result ? new Cup(data.now_cup) : null;
     data.url = EndPoint.Cups;
     return data;
@@ -128,16 +129,19 @@ async function GetCups(){
 
 
 const exportData = {
-    Classes:{
-        Rank,Team,Board,Rank
+    Classes: {
+        Rank,
+        Team,
+        Board,
+        Rank
     },
     GetCups,
     EndPoint
 };
 
-if(typeof window !== 'undefined'){
+if (typeof window !== 'undefined') {
     window.WBWrapper = exportData;
-}else{
+} else {
     module.exports = exportData;
 }
 
